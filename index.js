e3b0ab6d1ac927e5b497b0cb851ca26e40855b1c
martinzutelman <zutelmanmartin@gmail.com>
2023-10-24 12:17:26 -0300

    addded user skills
    created a skills table in the schema and connected it to the user, useers can have multiple skills in the database, I just need to change the code in the front end to process a user with multiple skills

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const express = require('express'); 
-const cookieParser = require('cookie-parser'); 
+const express = require('express');
+const cookieParser = require('cookie-parser');
 
 const app = express();
 const PORT = 3000;
--- routes/user.js
+++ routes/user.js
@@ -1,32 +1,47 @@
 require('dotenv').config();
 const authenticateToken = require('./authMiddleware');
-const express = require('express'); 
+const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 
-router.use (express.json());
+router.use(express.json());
+
+
 
 router.post('/', async (req, res) => {
-    try 
-    {  
+    try {
         const hashedPassword = await bcrypt.hash(req.body.password, 10)
-      
+
+        let skillOk = true;
+
+        checkAllSkills = req.body.skillId.forEach(async (skillId) => {
+            const checkEachSkill = await prisma.skill.findUnique({
+                where: {
+                    id: skillId
+                }
+            })
+            if(!checkEachSkill) {	
+                skillOk = false;
+                res.status(400).json({ error: 'One or more skills do not exist' });
+            }
+        })
+        if (skillOk == false) return res.status(400).json({ error: 'One or more skills do not exist'});
+
         await prisma.user.create({
             data: {
                 email: req.body.email,
                 password: hashedPassword,
-                userName: req.body.userName
+                userName: req.body.userName,
+                skills: { connect: [{id: req.body.skillId[0]}]  }
             }
         })
         res.status(201).send();
-    } 
-    catch (error) 
-    {
-        if (error.code === 'P2002') 
-        {
+    }
+    catch (error) {
+        if (error.code === 'P2002') {
             return res.status(400).json({ error: 'User with the same email or username already exists.' });
         }
         console.error('Error creating user:', error);
@@ -34,7 +49,7 @@ router.post('/', async (req, res) => {
     }
 })
 
-router.get ('/', authenticateToken, async (req, res) => {
+router.get('/', authenticateToken, async (req, res) => {
     const userInfo = await prisma.user.findUnique({
         where: {
             email: req.user.email
