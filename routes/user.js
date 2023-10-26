483736c6b38e19ddff8b842f1d34fb080f7395fd
martinzutelman <zutelmanmartin@gmail.com>
2023-10-26 16:00:18 -0300

    multiple skills
    added feature to be able to create a user with multiple skills and modified the get user route to respond with only the right info

--- routes/user.js
+++ routes/user.js
@@ -17,25 +17,27 @@ router.post('/', async (req, res) => {
 
         let skillOk = true;
 
-        checkAllSkills = req.body.skillId.forEach(async (skillId) => {
+        checkAllSkills = req.body.skillIds.forEach(async (skillId) => {
             const checkEachSkill = await prisma.skill.findUnique({
                 where: {
                     id: skillId
                 }
             })
-            if(!checkEachSkill) {	
+            if (!checkEachSkill) {
                 skillOk = false;
                 res.status(400).json({ error: 'One or more skills do not exist' });
             }
         })
-        if (skillOk == false) return res.status(400).json({ error: 'One or more skills do not exist'});
+        if (skillOk == false) return res.status(400).json({ error: 'One or more skills do not exist' });
 
         await prisma.user.create({
             data: {
                 email: req.body.email,
                 password: hashedPassword,
                 userName: req.body.userName,
-                skills: { connect: [{id: req.body.skillId[0]}]  }
+                skills: {
+                    connect: req.body.skillIds.map((id) => ({ id })),
+                }
             }
         })
         res.status(201).send();
@@ -53,6 +55,17 @@ router.get('/', authenticateToken, async (req, res) => {
     const userInfo = await prisma.user.findUnique({
         where: {
             email: req.user.email
+        },
+
+        select: {
+            id: true,
+            email: true,
+            userName: true,
+            skills: {
+                select: {
+                    name: true
+                }
+            }
         }
     })
     res.json(userInfo)
