cc50d5289753537fb6ea2b362ffda7c2a60d5bb4
BeadeA <agusbea05@gmail.com>
2023-11-07 13:48:26 -0300

    merge conflicts

--- components/projects/ProjectsScroll.tsx
+++ components/projects/ProjectsScroll.tsx
@@ -1,67 +1,87 @@
 import Hearts from "./Hearts"
 import LikeDislikeButton from "./LikeDislikeButton"
 import constants from "./constants"
+import { PrismaClient } from '@prisma/client';
+const prisma = new PrismaClient();
 
-const Project = (
-    {
-    title, 
-    description,
-    members,
-    joined,
-    state,
-    likes	
-    } : {
-    title : string | number,
-    description : string
-    members : number,
-    joined : number,
-    state : string,
-    likes : number
+
+const Project = ({
+  title,
+  description,
+  members,
+  joined,
+  state,
+  likes,
+}: {
+  title: string | number;
+  description: string;
+  members: number;
+  joined: number;
+  state: string;
+  likes: number;
 }) => {
-    let stateText;
-        if (state === "In Progress") {
-            stateText = " text-orange";
-        } else if (state === "Finished") {
-            stateText = " text-green";
-        } else if (state === "Abandoned") {
-            stateText = " text-red";
-        } else {
-            stateText = " text-purple-500";
-        }
-    return (
-        // Project
-        <section className="flex align-middle justify-center">
-            <div className="py-4 px-4 bg-gray-700 w-40 h-60 rounded-xl flex flex-col justify-between">
-                {/* Titulo y Descripcion */}
-                <div>  
-                    <h1 className="text-[1rem] font-semibold font-raleway">{title}</h1>
-                    <p className="text-xs text-[#8F8F8F]">{description}</p>
-                </div>
-                <div className="">
-                    {/* Members, Joined and State of the Project */}
-                    <h3 className="text-sm">Members: {members}</h3>
-                    <h3 className="text-sm mb-1">Joined: {joined}</h3>
-                    <h3 className={`text-sm font-bold${stateText}`}>{state}</h3>
-                </div>
-                <div className="">
-                    {/* Likeness */}
-                    <Hearts porcentaje={likes}/>
-                    <div className="flex flex-row justify-end mt-3 ">
-                        <LikeDislikeButton
-                            filled={constants.likeFilledPath}
-                            notFilled={constants.likeNotFilledPath}
-                            filledDislike={constants.dislikeFilledPath}
-                            notFilledDislike={constants.dislikeNotFilledPath}
-                        />
-                    </div>
-                </div>
-            </div>
-        </section>
-    );
-}
+  let stateText;
+  if (state === "In Progress") {
+    stateText = " text-orange";
+  } else if (state === "Finished") {
+    stateText = " text-green";
+  } else if (state === "Abandoned") {
+    stateText = " text-red";
+  } else {
+    stateText = " text-purple-500";
+  }
+  return (
+    // Project
+    <section className="flex align-middle justify-center">
+      <div className="py-4 px-4 bg-gray-700 w-40 h-60 rounded-xl flex flex-col justify-between">
+        {/* Titulo y Descripcion */}
+        <div>
+          <h1 className="text-[1rem] font-semibold font-raleway">{title}</h1>
+          <p className="text-xs text-[#8F8F8F]">{description}</p>
+        </div>
+        <div className="">
+          {/* Members, Joined and State of the Project */}
+          <h3 className="text-sm">Members: {members}</h3>
+          <h3 className="text-sm mb-1">Joined: {joined}</h3>
+          <h3 className={`text-sm font-bold${stateText}`}>{state}</h3>
+        </div>
+        <div className="">
+          {/* Likeness */}
+          <Hearts porcentaje={likes} />
+          <div className="flex flex-row justify-end mt-3 ">
+            <LikeDislikeButton
+              filled={constants.likeFilledPath}
+              notFilled={constants.likeNotFilledPath}
+              filledDislike={constants.dislikeFilledPath}
+              notFilledDislike={constants.dislikeNotFilledPath}
+            />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
 
-const ProjectsScroll = () => {
-    let amountProjects = 12;
+const ProjectsScroll = async () => {
+    const projectCount = await prisma.project.count();
+    for (let projectId = 0; projectId < projectCount; projectId++){
+     try {
+          const response = await fetch(`http://localhost:3000/project/getp`, {
+          method: "GET",
+          credentials: "include",
+          /*body: JSON.stringify({"projectId": projectId}) */
+        });
+  
+        const data = await response.json();
+        console.log(data)
+
+        if (!response.ok) {
+          console.log(data);
+        }
+      } catch (error) {console.log(error)
+        console.error();}
+    }
+    let amountProjects = projectCount;
     let projectNumber = 0;
     let amountMembers = 0;
     let amountLikes = 0;
