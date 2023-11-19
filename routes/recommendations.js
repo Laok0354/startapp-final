1032374e0665bbccd7f1f4d8632d9ff858c79fab
martinzutelman <zutelmanmartin@gmail.com>
2023-11-19 19:28:11 -0300

    project recommendations

---
+++ routes/recommendations.js
@@ -0,0 +1,82 @@
+require('dotenv').config();
+const authenticateToken = require('./authMiddleware');
+const express = require('express');
+const router = express.Router();
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
+
+router.use(express.json());
+
+
+router.post('/recommend', async (req, res) => {
+
+    const checkUser = await prisma.user.findUnique({
+        where: {
+            id: req.body.id
+        }
+    });
+
+    if (!checkUser) {return res.status(400).json({ error: 'User does not exist' })}
+
+    const checkProject = await prisma.project.findMany({
+        where: {
+            id: {
+                in: req.body.projectsWithAffinities.map(({ projectId }) => projectId),
+            },
+        },
+    });
+
+    if (checkProject.length !== req.body.projectsWithAffinities.length) {
+        return res.status(400).json({ error: 'One or more project IDs do not exist' });
+    }
+
+    const recommendedProjects = await prisma.recommendedProject.createMany({
+
+        data: req.body.projectsWithAffinities.map(({ projectId, affinity }) => ({
+            
+            projectId,
+            affinity, 
+            userId: req.body.id,
+
+
+        })),
+
+        skipDuplicates: true,
+
+    });
+
+    if (recommendedProjects === null) {
+        return res.status(500).send({message: "couldnt create recommendations"});
+    }
+    
+    res.status(200).json({message: "created recommendations"});
+
+});
+
+router.get('/getRecommended', authenticateToken, async (req, res) => {
+    const recommendedProjects = await prisma.recommendedProject.findMany({
+        where: {
+          userId: req.user.id, // Replace with the actual user ID
+        },
+        orderBy: {
+          affinity: 'desc', // Order by affinity in descending order (higher affinity first)
+        },
+    });
+
+    res = res.status(200).json(recommendedProjects);
+});
+
+router.delete('/deleteAllRecommendations', authenticateToken, async (req, res) => {
+    const dR = await prisma.recommendedProject.deleteMany({});
+
+    if(dR === null) {
+        res.status(500).send({message: "couldnt delete trcommmendations"});
+    }
+
+    res.status(200).send({message: "deleted all recommendations"});
+});
+
+module.exports = router;
+
